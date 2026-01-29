8a15c0442ae9ba65d5a22a27eb9c2cbdeab69388
Zotyamester <szatmary.zoltan1222@gmail.com>
2026-01-29 18:43:41 +0100

    Generalize the creation of subnets and hosts

--- net.py
+++ net.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
+import itertools
 from ipaddress import IPv4Interface, IPv4Network
-from mininet.topo import Topo
-from mininet.net import Mininet
+
 from mininet.cli import CLI
-from mininet.node import Node
 from mininet.link import TCLink
-from mininet.log import setLogLevel, info
+from mininet.log import info, setLogLevel
+from mininet.net import Mininet
+from mininet.node import Node
+from mininet.topo import Topo
 
 
 class PragueHost(Node):
@@ -28,66 +30,76 @@ class DualPI2Router(Node):
 
         for intf in self.intfList():
             self.cmd(f"ethtool -K {intf} tso off gso off gro off lro off")
-            # self.cmd(f"tc qdisc replace dev {intf} root dualpi2")
+            self.cmd(f"tc qdisc replace dev {intf} root dualpi2")
         self.cmd("sysctl -w net.ipv4.ip_forward=1")
 
 
 class L4STopo(Topo):
-    def build(self):
-        net192 = IPv4Network("192.168.0.0/24")
-        (r0_ip192, h1_ip, *_) = (
-            IPv4Interface(f"{ip}/{net192.prefixlen}") for ip in net192.hosts()
-        )
+    """
+    Represents a star topology with a single router (`r0`) connecting several (see `n_net`) switched LAN segments.
+    Each LAN consists of a single switch (`si`) connected to a handful (see `n_host`) of hosts (`hi`).
+    """
 
-        net172 = IPv4Network("172.16.0.0/24")
-        (r0_ip172, h2_ip, *_) = (
-            IPv4Interface(f"{ip}/{net192.prefixlen}") for ip in net172.hosts()
-        )
+    def build(self, n_net: int = 2, _n_host: int = 1):
+        BASE_SUBNET = IPv4Network("10.0.0.0/8")
 
-        router = self.addHost("r0", cls=DualPI2Router, ip=r0_ip192.with_prefixlen)
+        # make a list of `n_net` /24 subnets in the `BASE_SUBNET`
+        nets = [
+            tuple(
+                IPv4Interface(f"{ip}/{neti.prefixlen}") for ip in neti.hosts()
+            )  # a given subnet shall be immutable, hence the tuple
+            #    (How on Earth would you add new addresses belonging to the specified range anyway?)
+            for neti in itertools.islice(BASE_SUBNET.subnets(16), n_net)
+        ]
 
-        h1 = self.addHost(
-            "h1",
-            cls=PragueHost,
-            ip=h1_ip.with_prefixlen,
-            defaultRoute=f"via {r0_ip192.ip}",
-        )
-        h2 = self.addHost(
-            "h2",
-            cls=PragueHost,
-            ip=h2_ip.with_prefixlen,
-            defaultRoute=f"via {r0_ip172.ip}",
-        )
+        r0 = self.addHost(
+            "r0", cls=DualPI2Router, ip=nets[0][0].with_prefixlen
+        )  # specify the first subnet's first IP address in the `ip` param
 
-        s1, s2, *_ = (self.addSwitch(f"s{i}") for i in range(1, 2 + 1))
+        for i, (r0_i_ip, hi_ip, *_) in enumerate(nets, start=1):
+            si = self.addSwitch(f"s{i}")
 
-        self.addLink(
-            s1,
-            router,
-            intfName2="r0-eth1",
-            cls=TCLink,
-            params2={"ip": r0_ip192.with_prefixlen},
-        )
-        self.addLink(
-            s2,
-            router,
-            intfName2="r0-eth2",
-            cls=TCLink,
-            params2={"ip": r0_ip172.with_prefixlen},
-        )
+            self.addLink(
+                si,
+                r0,
+                intfName2=f"r0-eth{i}",
+                cls=TCLink,
+                params2={"ip": r0_i_ip.with_prefixlen},
+            )
 
-        for h, s in ((h1, s1), (h2, s2)):
-            self.addLink(h, s)
+            # TODO: here, we could make use of the `n_host` param, but it would require
+            #       a change in the naming scheme
+            hi = self.addHost(
+                f"h{i}",
+                cls=PragueHost,
+                ip=hi_ip.with_prefixlen,
+                defaultRoute=f"via {r0_i_ip.ip}",
+            )
+
+            self.addLink(hi, si)
 
 
-def run():
+def run(benchmark: bool = False):
+    """
+    Setup the `L4STopology` with the specified parameters and benchmark the performance or show the interactive
+    Mininet console.
+
+    :param benchmark: Perform benchmarks instead of entering to CLI mode.
+    """
     topo = L4STopo()
     net = Mininet(topo=topo, waitConnected=True)
     net.start()
 
     info("*** Routing Table on Router (r0) ***\n")
     info(net["r0"].cmd("ip route"))
-    CLI(net)
+
+    if benchmark:
+        pass  # TODO: do pingall, iperf, etc.
+        # Q: Shall this script perform a comprehensive benchmark with different CC & AQM methods on its own?
+        #    Or maybe it can rely on external tools for parameterizing this script, and thus it should only be
+        #    concerned about conducting measurement runs for a single case (i.e., with fixed CC & AQM methods).
+    else:
+        CLI(net)
 
     net.stop()
 
